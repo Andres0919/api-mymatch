fb7eba5ff72c83c32771ace527390d8c2f31968d
Andrés Posada <andres.posada0919@gmail.com>
2020-06-15 17:20:13 -0500

    added db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 const Hapi = require('@hapi/hapi')
+const { Client } = require('pg');
 
 const init = async () => {
 	const server = Hapi.Server({
 		port: process.env.PORT || 5000,
 		host:'0.0.0.0'
-	})
+  })
+    
+	const client = new Client({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false
+		}
+	});
+
+	client.connect();
+
+	client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+		if (err) throw err;
+		for (let row of res.rows) {
+			console.log(JSON.stringify(row));
+		}
+		client.end();
+	});
+
 
 	await server.start()
 
