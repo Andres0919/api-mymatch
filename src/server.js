fbfcbdd7db0529c2c799d2f4f84d274279c11e2f
Andrés Posada <andres.posada0919@gmail.com>
2020-06-15 16:17:50 -0500

    change host

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const Hapi = require('@hapi/hapi')
 const init = async () => {
 	const server = Hapi.Server({
 		port: process.env.PORT || 5000,
-		host: 'localhost'
+		host:'0.0.0.0'
 	})
 
 	await server.start()
