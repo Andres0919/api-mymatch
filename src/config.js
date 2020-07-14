3cc00aad85f3957affec695141cd10e50eabf27d
Andrés Posada <andres.posada0919@gmail.com>
2020-07-14 06:40:28 -0500

    change env port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   node_env: process.env.NODE_ENV || 'dev',
   api: {
-    port: process.env.API_PORT || 5000,
+    port: process.env.PORT || 5000,
   },
   db_uri: process.env.DATABASE_URL || '',
 }
