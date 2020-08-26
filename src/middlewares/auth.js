6fefc3a2b56962da774dbf960725e5bd5db9445d
Andres Posada <andres.posada0919@gmail.com>
2020-08-25 21:44:42 -0500

    refactoring auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,62 +4,56 @@ const moment = require('../utils/moment')
 
 const isAuthenticated = async (req, res, next) => {
   const { authorization } = req.headers
-  try {
-    if (!authorization) {
-      return response.error(
-        req,
-        res,
-        {
-          message:
-            'I need to know who are you, you send me the authorization header',
-        },
-        401
-      )
-    }
-
-    const [Bearer, token] = authorization.split(' ')
-    if (Bearer !== 'Bearer' || !token) {
-      return response.error(
-        req,
-        res,
-        {
-          message: `I'm sorry, the token no valid`,
-        },
-        401
-      )
-    }
-
-    let bodyToken
-    bodyToken = decodeToken(token)
-
-    if (!bodyToken || !bodyToken.portfolio || !bodyToken.expires_at) {
-      return response.error(
-        req,
-        res,
-        {
-          message: `I'm sorry, the authorization token no valid`,
-        },
-        401
-      )
-    }
-
-    if (bodyToken.expires_at <= moment().unix()) {
-      return response.error(
-        req,
-        res,
-        {
-          message: `I'm sorry, the token has expired`,
-        },
-        401
-      )
-    }
-
-    next()
-  } catch ({ message }) {
-    response.error(req, res, {
-      message,
-    })
+  if (!authorization) {
+    return response.error(
+      req,
+      res,
+      {
+        message:
+          'I need to know who are you, you send me the authorization header',
+      },
+      401
+    )
   }
+
+  const [Bearer, token] = authorization.split(' ')
+  if (Bearer !== 'Bearer' || !token) {
+    return response.error(
+      req,
+      res,
+      {
+        message: `I'm sorry, the token no valid`,
+      },
+      401
+    )
+  }
+
+  let bodyToken
+  bodyToken = decodeToken(token)
+
+  if (!bodyToken || !bodyToken.portfolio || !bodyToken.expires_at) {
+    return response.error(
+      req,
+      res,
+      {
+        message: `I'm sorry, the authorization token no valid`,
+      },
+      401
+    )
+  }
+
+  if (bodyToken.expires_at <= moment().unix()) {
+    return response.error(
+      req,
+      res,
+      {
+        message: `I'm sorry, the token has expired`,
+      },
+      401
+    )
+  }
+
+  next()
 }
 
 module.exports = {
